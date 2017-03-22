08ce453eecf1b01273a4c82a9b44084667ec48d0
Marcial <Marcialararacap@hotmail.com>
2017-03-21 17:16:14 -0700

    update

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer>
+<ul id="sitemap">
+    <li>Home</li>
+    <li>About</li>
+    <li>Learn Equipment</li>
+    <li>Exercises</li>
+    <li>Fitness Tools</li>
+    <li>Vlogs</li>
+    
+</ul>
+    
+<ul id="footer-info">
+    <li>Business Inquiries: Business@gmail.com</li>
+    <li>Youtube</li>
+    <li>Instagram</li>
+    <li>Facebook</li>
+<br />
+    <p id="copyrights">2017 Iron Alliance</p>
+</ul>
+    
+</footer>
+/*
+
+Credits to the authors of:
+--------------------------
+
+Applies to all CSS and LESS files in the following directories: font-awesome/css/, font-awesome/less/, and font-awesome/scss/.
+License: MIT License
+URL: http://opensource.org/licenses/mit-license.html
+
+Applies to all Font Awesome project files that are not a part of the Font or Code licenses.
+License: CC BY 3.0
+URL: http://creativecommons.org/licenses/by/3.0/
+*/
---
+++ navbar.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Iron Alliance</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="CSS/style.css" rel="stylesheet">
+      <link href="css/full-slider.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Acme|Baloo+Bhaina" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+    <link href="css/carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav id="nav" class="navbar navbar-default">
+          <div class="container">
+            <div class="navbar-header">
+              
+              <a id="brandname" class="navbar-brand" href="#">Iron Alliance</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#equipment">Learn Equipment</a></li>
+                <li><a href="#exercises">Exercises</a></li>
+                <li><a href="#fitness">Fitness Tools</a></li>
+                <li><a href="#vlogs">Vlogs</a></li>
+                <li><a id="fb" class="btn btn-social-icon btn-facebook" href="#fb"><span  class="fa fa-facebook-official"></span></a></li>
+                <li><a id="ig" class="fb btn btn-social-icon btn-instagram" href="#fb"><span class="fa fa-instagram"></span></a></li>
+                <li><a id="yt" class="btn btn-social-icon btn-youtube-play" href="#fb"><span class="fa fa-youtube-play"></span></a></li>
+                
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
